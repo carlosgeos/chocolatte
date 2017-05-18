b650373bef545462bfa10603e5d74e2dc8bd76eb
Perale Thomas <thomas.perale@openmailbox.org>
2017-05-18 23:03:21 +0200

    Knight fix + methods (still have to improve, code still reduceable)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,7 +5,7 @@ import org.chocosolver.solver.search.strategy.Search;
 import org.chocosolver.solver.variables.IntVar;
 
 public class App {
-    public void chess(int n) {
+    public void queenl(int n) {
         Model model = new Model(n + "-queens problem");
         IntVar[] vars = new IntVar[n];
 
@@ -23,6 +23,108 @@ public class App {
         Solution solution = model.getSolver().findSolution();
         if (solution != null){
             System.out.println(solution.toString());
+        }
+    }
+
+    public void domination(int n, int rook, int bishop, int knight) {
+        Model model = new Model("Domination problem");
+
+        // Creation of rooks
+        IntVar[][] rooks = new IntVar[rook][2];
+        for (int r = 0; r < rook; r++) {
+            rooks[r][0] = model.intVar("R_" + r + "_x", 0, n - 1);
+            rooks[r][1] = model.intVar("R_" + r + "_y", 0, n - 1);
+        }
+
+        // Creation of bishops
+        IntVar[][] bishops = new IntVar[bishop][2];
+        for (int b = 0; b < bishop; b++) {
+            bishops[b][0] = model.intVar("B_" + b + "_x", 0, n - 1);
+            bishops[b][1] = model.intVar("B_" + b + "_y", 0, n - 1);
+        }
+
+        // Creation of knights
+        IntVar[][] knights = new IntVar[knight][2];
+        for (int k = 0; k < knight; k++) {
+            knights[k][0] = model.intVar("K_" + k + "_x", 0, n - 1);
+            knights[k][1] = model.intVar("K_" + k + "_y", 0, n - 1);
+        }
+
+        // TODO Condition creation.
+    }
+
+    private void rook_constraints(Model model, IntVar[] a, IntVar[] b) {
+        model.arithm(a[0], "!=", b[0]).post();
+        model.arithm(a[1], "!=", b[1]).post();
+    }
+
+    private void rooks_constraints(Model model, IntVar[] current, IntVar[][] other) {
+        for (int j = 0; j < other.length; j++) {
+            rook_constraints(model, current, other[j]);
+        }
+    }
+
+    private void bishop_constraints(Model model, IntVar[] a, IntVar[] b) {
+        IntVar y = a[1].sub(b[1]).abs().intVar();
+        model.not(model.distance(a[0], b[0], "=", y)).post();
+    }
+
+    private void bishops_constraints(Model model, IntVar[] current, IntVar[][] other) {
+        for (int j = 0; j < other.length; j++) {
+            bishop_constraints(model, current, other[j]);
+        }
+    }
+
+    private void knight_constraints(Model model, IntVar[] a, IntVar[] b) {
+        model.or(
+            model.arithm(a[0], "!=", b[0]),
+            model.arithm(a[1], "!=", b[1])
+        ).post();
+
+        model.or(
+            model.arithm(a[0], "!=", b[0], "+", 2),
+            model.arithm(a[1], "!=", b[1], "+", 1)
+        ).post();
+
+        model.or(
+            model.arithm(a[0], "!=", b[0], "+", 2),
+            model.arithm(a[1], "!=", b[1], "-", 1)
+        ).post();
+
+        model.or(
+            model.arithm(a[0], "!=", b[0], "-", 2),
+            model.arithm(a[1], "!=", b[1], "-", 1)
+        ).post();
+
+        model.or(
+            model.arithm(a[0], "!=", b[0], "-", 2),
+            model.arithm(a[1], "!=", b[1], "+", 1)
+        ).post();
+
+        model.or(
+            model.arithm(a[0], "!=", b[0], "+", 1),
+            model.arithm(a[1], "!=", b[1], "+", 2)
+        ).post();
+
+        model.or(
+            model.arithm(a[0], "!=", b[0], "+", 1),
+            model.arithm(a[1], "!=", b[1], "-", 2)
+        ).post();
+
+        model.or(
+            model.arithm(a[0], "!=", b[0], "-", 1),
+            model.arithm(a[1], "!=", b[1], "-", 2)
+        ).post();
+
+        model.or(
+            model.arithm(a[0], "!=", b[0], "-", 1),
+            model.arithm(a[1], "!=", b[1], "+", 2)
+        ).post();
+    }
+
+    private void knights_constraints(Model model, IntVar[] current, IntVar[][] other) {
+        for (int j = 0; j < other.length; j++) {
+            knight_constraints(model, current, other[j]);
         }
     }
 
@@ -53,82 +155,31 @@ public class App {
         // Conditions on rooks
         for (int i = 0; i < rook; i++) {
             for (int j = i + 1; j < rook; j++) {
-                model.arithm(rooks[i][0], "!=", rooks[j][0]).post();
-                model.arithm(rooks[i][1], "!=", rooks[j][1]).post();
+                rook_constraints(model, rooks[i], rooks[j]);
             }
 
-            for (int j = 0; j < bishop; j++) {
-                model.arithm(rooks[i][0], "!=", bishops[j][0]).post();
-                model.arithm(rooks[i][1], "!=", bishops[j][1]).post();
-            }
-
-            for (int j = 0; j < knight; j++) {
-                model.arithm(rooks[i][0], "!=", knights[j][0]).post();
-                model.arithm(rooks[i][1], "!=", knights[j][1]).post();
-            }
+            rooks_constraints(model, rooks[i], bishops);
+            rooks_constraints(model, rooks[i], knights);
         }
 
         // Conditions on bishops
         for (int i = 0; i < bishop; i++) {
             for (int j = i + 1; j < bishop; j++) {
-                IntVar y = bishops[i][1].sub(bishops[j][1]).abs().intVar();
-                model.not(model.distance(bishops[i][0], bishops[j][0], "=", y)).post();
+                bishop_constraints(model, bishops[i], bishops[j]);
             }
 
-            for (int j = 0; j < rook; j++) {
-                IntVar y = bishops[i][1].sub(rooks[j][1]).abs().intVar();
-                model.not(model.distance(bishops[i][0], rooks[j][0], "=", y)).post();
-            }
-
-            for (int j = 0; j < knight; j++) {
-                IntVar y = bishops[i][1].sub(knights[j][1]).abs().intVar();
-                model.not(model.distance(bishops[i][0], knights[j][0], "=", y)).post();
-            }
+            bishops_constraints(model, bishops[i], rooks);
+            bishops_constraints(model, bishops[i], knights);
         }
 
         // Conditions on knights
         for (int i = 0; i < knight; i++) {
             for (int j = i + 1; j < knight; j++) {
-                model.and(
-                    model.arithm(knights[i][0], "!=", knights[j][0], "+", 2),
-                    model.arithm(knights[i][1], "!=", knights[j][1], "+", 1)
-                ).post();
-
-                model.and(
-                    model.arithm(knights[i][0], "!=", knights[j][0], "+", 2),
-                    model.arithm(knights[i][1], "!=", knights[j][1], "-", 1)
-                ).post();
-
-                model.and(
-                    model.arithm(knights[i][0], "!=", knights[j][0], "-", 2),
-                    model.arithm(knights[i][1], "!=", knights[j][1], "-", 1)
-                ).post();
-
-                model.and(
-                    model.arithm(knights[i][0], "!=", knights[j][0], "-", 2),
-                    model.arithm(knights[i][1], "!=", knights[j][1], "+", 1)
-                ).post();
-
-                model.and(
-                    model.arithm(knights[i][0], "!=", knights[j][0], "+", 1),
-                    model.arithm(knights[i][1], "!=", knights[j][1], "+", 2)
-                ).post();
-
-                model.and(
-                    model.arithm(knights[i][0], "!=", knights[j][0], "+", 1),
-                    model.arithm(knights[i][1], "!=", knights[j][1], "-", 2)
-                ).post();
-
-                model.and(
-                    model.arithm(knights[i][0], "!=", knights[j][0], "-", 1),
-                    model.arithm(knights[i][1], "!=", knights[j][1], "-", 2)
-                ).post();
-
-                model.and(
-                    model.arithm(knights[i][0], "!=", knights[j][0], "-", 1),
-                    model.arithm(knights[i][1], "!=", knights[j][1], "+", 2)
-                ).post();
+                knight_constraints(model, knights[i], knights[j]);
             }
+
+            knights_constraints(model, knights[i], rooks);
+            knights_constraints(model, knights[i], bishops);
         }
 
         Solution solution = model.getSolver().findSolution();
